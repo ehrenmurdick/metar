aae49dd47b58654c7cdedfc59446e07e97a0694e
Ehren Murdick <ehren.murdick@gmail.com>
2010-10-24 18:27:55 -0700

    added memcached

--- metar.rb
+++ metar.rb
@@ -1,23 +1,34 @@
 require 'rubygems'
-
-require 'mechanize'
-
 require 'sinatra'
+
+configure do
+  require 'mechanize'
+  require 'memcached'
+
+  CACHE = Memcached.new
+end
 
 
 get "/:airport" do
   break unless params[:airport] != ""
-  addy = "http://aviationweather.gov/adds/metars/"
-  mech = ::Mechanize.new { |agent| agent.user_agent_alias = 'Mac Safari' }
-  result = nil
-  mech.get(addy) do |page|
-    result = page.form_with(:action => "/adds/metars/index.php") do |form|
-      form.station_ids = params[:airport]
-    end.submit
-
-  end
 
   content_type "text/plain"
-  result.parser.css('font').map {|f| f.text }.join("\n\n")
+
+  begin
+    CACHE.get(params[:airport])
+  rescue Memcached::NotFound
+    addy = "http://aviationweather.gov/adds/metars/"
+
+    mech = ::Mechanize.new { |agent| agent.user_agent_alias = 'Mac Safari' }
+    result = nil
+    mech.get(addy) do |page|
+      result = page.form_with(:action => "/adds/metars/index.php") do |form|
+        form.station_ids = params[:airport]
+      end.submit
+    end
+    metar = result.parser.css('font').map {|f| f.text }.join("\n\n")
+    CACHE.set(params[:airport], metar, 3600)
+    metar
+  end
 end
 
